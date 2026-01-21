6d84bdb7d6e54e6d2681c07d8de42b945f6d8332
Isabella Orellana <124645010+isabellaorellana@users.noreply.github.com>
2026-01-21 18:29:36 -0300

    Update deposit.js pequeño error

--- HTML/deposit.js
+++ HTML/deposit.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
 
     $('#depositForm').submit(function(e) {
         e.preventDefault();
-        
         $('#alert-container').empty();
 
         const montoIngresado = Number($('#montoNuevo').val());
@@ -21,10 +20,17 @@ $(document).ready(function() {
             mostrarAlerta("<strong>Error:</strong> Ingresa un monto válido mayor a 0.", "danger");
             return;
         }
+
         let saldoViejo = Number(localStorage.getItem("saldo")) || 60000;
         let nuevoSaldo = saldoViejo + montoIngresado;
-        
         localStorage.setItem("saldo", nuevoSaldo);
+
+        let movimientosReales = JSON.parse(localStorage.getItem("movimientos")) || [];
+    
+        const nuevoMovimiento = `Depósito de $${montoIngresado}`;
+        movimientosReales.push(nuevoMovimiento);
+        
+        localStorage.setItem("movimientos", JSON.stringify(movimientosReales));
         mostrarAlerta(`<strong>¡Depósito exitoso!</strong> Has sumado $${montoIngresado.toLocaleString()}.`, "success");
         $('#saldoActual').text(`$${nuevoSaldo.toLocaleString()}`);
         $('#montoNuevo').val('');
